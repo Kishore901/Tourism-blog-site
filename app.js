e8ecb7fed3b362421693b2505c81fb085d1b2204
Kishore901 <getkishore3008@gmail.com>
2021-04-10 11:23:26 +0530

    Added navbar using bootstrap.

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
+// express app
 const app = express();
 
+// register view engine
 app.set('view engine', 'ejs');
+
+// listen to request
 app.listen(3000);
+
+// static files
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('home');
