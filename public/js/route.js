3367f937d100534981877928fbd740c28ce17247
Kishore901 <getkishore3008@gmail.com>
2021-04-27 10:47:19 +0530

    Added image functionality.

--- public/js/route.js
+++ public/js/route.js
@@ -1,37 +1,43 @@
 //require everything here
-const sidenavbar = document.querySelector(".sidenavbar");
-const Contentdiv = document.querySelector(".Contentdiv");
-const sidenavbaricon = document.querySelector(".sidenavbaricon");
+const sidenavbar = document.querySelector('.sidenavbar');
+const Contentdiv = document.querySelector('.Content-div');
+const sidenavbaricon = document.querySelector('.sidenavbaricon');
+const contentDivImage1 = document.querySelector('.content-img1');
 
-sidenavbar.addEventListener("click", (e) => {
-    if (e.target.classList.contains("placelist")) {
-        console.log(e.target.textContent);
-        fetchcontent(e.target.textContent)
-    }
-})
+sidenavbar.addEventListener('click', (e) => {
+  if (e.target.classList.contains('placelist')) {
+    console.log(e.target.textContent);
+    fetchcontent(e.target.textContent);
+  }
+});
 
-sidenavbaricon.addEventListener("click",()=>{
-    console.log("here");
-    if(sidenavbar.classList.contains("vanishsidenavbar")){
-        sidenavbar.classList.remove("vanishsidenavbar");
-        sidenavbaricon.innerHTML = `<i class="icon fas fa-chevron-left"></i>`
-    }
-    else{
-        sidenavbar.classList.add("vanishsidenavbar");
-        sidenavbaricon.innerHTML = `<i class="icon fas fa-chevron-right"></i>`
-
-    }
-
-})
+sidenavbaricon.addEventListener('click', () => {
+  console.log('here');
+  if (sidenavbar.classList.contains('vanishsidenavbar')) {
+    sidenavbar.classList.remove('vanishsidenavbar');
+    sidenavbaricon.innerHTML = `<i class="icon fas fa-chevron-left"></i>`;
+  } else {
+    sidenavbar.classList.add('vanishsidenavbar');
+    sidenavbaricon.innerHTML = `<i class="icon fas fa-chevron-right"></i>`;
+  }
+});
 
 //function to display content
-function displaycontent(content){
-    Contentdiv.textContent = content;
+function displaycontent(content) {
+  console.log(contentDivImage1);
+  if (contentDivImage1.classList.contains('content-img1-hidden')) {
+    console.log(contentDivImage1);
+    contentDivImage1.classList.remove('content-img1-hidden');
+    contentDivImage1.classList.add('content-img1-show');
+  }
+  //   I have added some height and widht to the images, later we can add a div around it and make it proper
+  contentDivImage1.src = content.imgsrc;
+  Contentdiv.textContent = content.hello;
 }
 
 //function to fetch content
-async function fetchcontent(content){
-    const fetchedcontent =  await fetch(`/JSON/${content}.json`)
-    const jsoncontent = await fetchedcontent.json();
-    await displaycontent(jsoncontent.hello);
+async function fetchcontent(content) {
+  const fetchedcontent = await fetch(`/JSON/${content}.json`);
+  const jsoncontent = await fetchedcontent.json();
+  await displaycontent(jsoncontent);
 }
